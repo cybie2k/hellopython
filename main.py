f90618f674b19aa79134e5835ed90851658e2d77
cybie2k <cybie2k@gmail.com>
2022-11-28 05:23:37 -0500

    Cleaned up the output. saved results as an XLSX file.

--- main.py
+++ main.py
@@ -50,47 +50,14 @@ TimeLogData["Event Duration"]=(TimeLogData["Clock Out"]-TimeLogData["Clock In"])
 TimeLogData["Work Day"]=pd.to_datetime(TimeLogData['Clock In']-pd.Timedelta(hours=7)).dt.date
 TimeLogData["Work Day of Week"]=pd.to_datetime(TimeLogData["Work Day"]).dt.day_of_week
 TimeLogData["Temp"]=np.where(TimeLogData["Work Day of Week"]==6,0,(TimeLogData["Work Day of Week"]+1)*-1).astype(int)
-TimeLogData["Work Week"]=(TimeLogData["Work Day"] - pd.to_datetime(TimeLogData["Work Day"]).dt.weekday * np.timedelta64(1,'D'))-pd.Timedelta(days=1)
-##logging.debug(frames)
+TimeLogData["Work Week"]=pd.to_datetime((pd.to_datetime(TimeLogData["Work Day"]) - pd.to_datetime(TimeLogData["Work Day"]).dt.weekday * np.timedelta64(1,'D'))-pd.Timedelta(days=1)).dt.date
+#logging.debug("Reindexing TimeLogData")
+#TimeLogData=TimeLogData.set_index("Clock In")
 
+logging.debug('Cleaning up the columns')
+TimeLogData=TimeLogData[["Work Week","Work Day","Clock In","Clock Out","Task","Event Duration","Notes"]]
+logging.debug("Reindexing TimeLogData")
+TimeLogData=TimeLogData.set_index(["Work Week","Work Day","Clock In"])
 logging.debug(TimeLogData)
+TimeLogData.to_excel("output.xlsx")
 
-
-
-
-
-
-
-
-""" logging.info(f"timelogdir = {timelogdir}\ntimelogfile = {timelogfile}")
-os.chdir(os.path.normpath(timelogdir))
-logging.info(f"Change directory to {os.path.normpath(timelogdir)}")
-wb = load_workbook(timelogfile,data_only=True)
-ws = wb.worksheets[0]
-logging.info(f"Loaded workbook {timelogfile}\nprocessing worksheet:{wb.worksheets[0]}")
-
-for row in ws.iter_rows(min_row=1, max_row=1,values_only=True):
-    rowheader=list(row)
-
-pp.pprint(rowheader)
-
-rownum=0
-allrows={}
-
-for row in ws.iter_rows(min_row=2,values_only=True):
-    rowid=str(rownum)
-    rowdata=list(row)
-    zippedrow={rowheader:rowdata for rowheader,rowdata in zip(rowheader,rowdata)}
-    ##allrows.append(zippedrow)
-    edur=zippedrow['Clock Out']-zippedrow['Clock In']
-    zippedrow['Event duration']=edur.seconds/3600
-    allrows[rowid]=zippedrow
-    ##print (rowid,'=',zippedrow)
-  
-    ##allrows{rownum}=zippedrow
-    rownum+=1
-##pp.pprint (allrows)
-logging.info(f'Closing {timelogfile}')
-wb.close
-logging.info('Done!')
-logging.warning('Danger! Danger! Will Robinson.') """
